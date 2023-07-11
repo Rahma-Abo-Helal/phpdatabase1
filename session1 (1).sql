06e10cbeae7d01933df320e744cad79891e70dde
Rahma-Abo-Helal <117909553+Rahma-Abo-Helal@users.noreply.github.com>
2023-07-11 04:07:09 +0300

    version-1

---
+++ session1 (1).sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 11, 2023 at 02:44 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ieee_sessions`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `session1`
+--
+
+CREATE TABLE `session1` (
+  `name` varchar(10) NOT NULL,
+  `age` int(2) NOT NULL,
+  `phone` int(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `id` varchar(20) NOT NULL,
+  `college` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `session1`
+--
+ALTER TABLE `session1`
+  ADD UNIQUE KEY `name` (`name`,`email`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
